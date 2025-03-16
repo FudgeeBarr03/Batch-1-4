47b6c364bd2714e9ce37b356872003db453a8525
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-03-16 20:48:44 +0800

    ouput print (and corrected error in for range

--- Print_the_result_of_the_first_number_minus_all_of_the_remaining_numbers.py
+++ Print_the_result_of_the_first_number_minus_all_of_the_remaining_numbers.py
@@ -2,11 +2,12 @@
 numbers = []
 for i in range(10):
     num = int(input("enter a number: "))
-    numbers.append
+    numbers.append(num)
     
 #subtract first number from all the other numbers
-result = numbers [0]
+result = numbers[0]
 for num in numbers[1:]:
     result -= num
 
 #print difference
+print("The first number minus the remaining numbers is ", result)
