a6e1e6e8b9bcf7fef3ab2125cdc9a0d4d72a588d
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-03-16 23:30:02 +0800

    checking fopr duplicated numbers

--- Ask_for_10_numbers_and_display_non_duplicate_numbers.py
+++ Ask_for_10_numbers_and_display_non_duplicate_numbers.py
@@ -6,4 +6,8 @@ for i in range(10):
     numbers.append (num) #store numbers
 
 #check for duplicate numbers
+unique_numbers = []
+for num in numbers:
+    if numbers.count(num) == 1:
+        unique_numbers.append(num)
 #print numbers that do not have a duplicate
