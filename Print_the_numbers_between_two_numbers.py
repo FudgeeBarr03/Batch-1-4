d00885ff57c5324d62e37e9114cfaad4c7002424
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-03-16 21:40:46 +0800

    input numbers

--- Print_the_numbers_between_two_numbers.py
+++ Print_the_numbers_between_two_numbers.py
@@ -1,2 +1,5 @@
-#input 2 numbers
+#input 2 numbersnum1 = int(input("Enter 1st number: "))
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
 #print numbers in between the two
