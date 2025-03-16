08780f459f19b1265a247022818e9671180ef8a5
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-03-16 20:58:14 +0800

    numbers input range

--- In_10_numbers_input_how_many_are_even.py
+++ In_10_numbers_input_how_many_are_even.py
@@ -1,2 +1,9 @@
 #input 10 numbers
+even_numbers = 0
+
+for i in range(10):
+    num = int(input("enter a number: "))
+    if num % 2 == 0:
+        even_numbers += 1
+
 #output how many odd numbers are there
