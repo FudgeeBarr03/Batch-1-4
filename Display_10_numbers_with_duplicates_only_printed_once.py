5979fbeaed58b4609ba1415e090896efd34371bd
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-03-16 23:47:47 +0800

    first and duplicate number checking

--- Display_10_numbers_with_duplicates_only_printed_once.py
+++ Display_10_numbers_with_duplicates_only_printed_once.py
@@ -2,8 +2,14 @@
 numbers = []
 for i in range(10):
     num = int(input("Enter a number: "))
-    numbers.append(num)
-    
-#store values
-#check for duplicated values
+    numbers.append(num)     #store values
+
+#check for duplicated values and store first instance
+first_time_num = []
+duplicate_numbers = []
+for num in numbers:
+    if num not in duplicate_numbers:
+        first_time_num.append(num)
+        duplicate_numbers.append(num)
+
 #print duplicate values once
