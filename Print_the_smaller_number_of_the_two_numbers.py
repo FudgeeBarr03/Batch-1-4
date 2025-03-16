240f88666efcb580f246c2cd81d309e16c50057a
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-03-16 19:32:41 +0800

    imput numbers

--- Print_the_smaller_number_of_the_two_numbers.py
+++ Print_the_smaller_number_of_the_two_numbers.py
@@ -3,7 +3,3 @@ num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
 
 #output the smaller one
-if num1 < num2:
-    print(f"{num1} is smaller than {num2}")
-else:
-    print(f"{num2} is smaller than {num1}")
