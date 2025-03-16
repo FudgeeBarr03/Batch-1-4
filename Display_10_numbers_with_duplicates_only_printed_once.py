6578deebff3b0acff93bdd4e74a7a72e86552dd1
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-03-16 23:41:36 +0800

    user input

--- Display_10_numbers_with_duplicates_only_printed_once.py
+++ Display_10_numbers_with_duplicates_only_printed_once.py
@@ -1,4 +1,9 @@
 #ask for user input
+numbers = []
+for i in range(10):
+    num = int(input("Enter a number: "))
+    numbers.append(num)
+    
 #store values
 #check for duplicated values
 #print duplicate values once
