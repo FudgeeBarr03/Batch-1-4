713a3b68221e30e280eea64ca6731929d8c62772
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-03-16 20:44:09 +0800

    10 numbers input

--- Print_the_result_of_the_first_number_minus_all_of_the_remaining_numbers.py
+++ Print_the_result_of_the_first_number_minus_all_of_the_remaining_numbers.py
@@ -1,3 +1,8 @@
 #Ask for 10 numbers input
+numbers = []
+for i in range(10):
+    num = int(input("enter a number: "))
+    numbers.append
+    
 #subtract first number from all the other numbers
 #print difference
