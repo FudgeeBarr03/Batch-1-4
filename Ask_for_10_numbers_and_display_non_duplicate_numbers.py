250f5a6a7be3023e9a321c1d1f0f957b3df9f1ab
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-03-16 23:25:10 +0800

    user input

--- Ask_for_10_numbers_and_display_non_duplicate_numbers.py
+++ Ask_for_10_numbers_and_display_non_duplicate_numbers.py
@@ -1,5 +1,9 @@
 #initialize numbers
 #ask for input
-#store numbers
+numbers = []
+for i in range(10):
+    num = int(input("enter a number: "))
+    numbers.append (num) #store numbers
+
 #check for duplicate numbers
 #print numbers that do not have a duplicate
