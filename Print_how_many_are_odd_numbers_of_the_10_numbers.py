1ab88a310b4052a86bd30fbe9dae270d5ee6950d
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-03-16 18:07:22 +0800

    input numbers and see how many are odd

--- Print_how_many_are_odd_numbers_of_the_10_numbers.py
+++ Print_how_many_are_odd_numbers_of_the_10_numbers.py
@@ -1,3 +1,10 @@
-#initialize numbers
+#initialize odd numbers count
+odd_numbers = 0
+
 #input 10 numbers
+for i in range(10):
+    num = float(input("enter a number: "))
+    if num % 2 != 0:
+        odd_numbers +=1
+        
 #output odd numbers
