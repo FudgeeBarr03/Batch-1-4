bd255347cc46d90ca88dd9ff3d4b371fc8bd77a4
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-03-16 20:16:38 +0800

    output not equal

--- print_not_equal_if_two_numbers_arent_the_same.py
+++ print_not_equal_if_two_numbers_arent_the_same.py
@@ -4,3 +4,5 @@ num2 = int(input("Enter 2nd number: "))
 
 #check if two numbers are not the same
 #print "numbers are not equal" if they are not the same
+if num1 != num2:
+    print(f"{num1} and {num2} are not equal")
