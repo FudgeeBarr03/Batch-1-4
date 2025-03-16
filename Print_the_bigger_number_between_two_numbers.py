3029f01727fb24a410266383643cba50b1b67348
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-03-16 16:42:55 +0800

    Print Bigger Number

--- Print_the_bigger_number_between_two_numbers.py
+++ Print_the_bigger_number_between_two_numbers.py
@@ -2,4 +2,9 @@
 num1 = int(input("please input a number "))
 #input number 2
 num2 = int(input("input a second number "))
+
 #print the bigger number 
+if num1 > num2:
+    print(f"{num1} is the bigger number")
+else:
+    print(f"{num2} is the bigger number")
