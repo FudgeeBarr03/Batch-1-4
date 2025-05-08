dbc0c69827c15d4708922e203d29f47ba3aeb072
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-05-08 18:08:42 +0800

    ask for user input

--- ask_user_for_for_valid_nbumber_inputs_and_print_the_highest_number.py
+++ ask_user_for_for_valid_nbumber_inputs_and_print_the_highest_number.py
@@ -1,4 +1,9 @@
 #create an initial library for numbers
+numbers = []
+
 #create a loop asking for user input
+while True:
+    num = int(input("Enter a number:"))
+    numbers.append(num)
 #add a checker if number is a duplicate or if its unique
 #if user enters an invalid input, break
