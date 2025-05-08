67406c16c39449c7fadf7d54412651785d3f3131
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-05-08 18:12:48 +0800

    check to see if the user enters a valid input. if not, the program will terminate

--- ask_user_for_for_valid_nbumber_inputs_and_print_the_highest_number.py
+++ ask_user_for_for_valid_nbumber_inputs_and_print_the_highest_number.py
@@ -3,11 +3,15 @@ numbers = []
 
 #create a loop asking for user input
 while True:
-    num = int(input("Enter a number:"))
-    numbers.append(num)
-#add a checker if number is a duplicate or if its unique
-    if numbers.count(num) > 1:
-        print("Duplicate")
-    else:
-        print("Unique")
+    try:
+        num = int(input("Enter a number:"))
+        numbers.append(num)
+    #add a checker if number is a duplicate or if its unique
+        if numbers.count(num) > 1:
+            print("Duplicate")
+        else:
+            print("Unique")
 #if user enters an invalid input, break
+    except ValueError:
+        print("Input is invalid. Terminating program.")
+        break
