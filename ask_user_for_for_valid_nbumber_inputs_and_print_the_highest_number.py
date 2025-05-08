85d20070274fd06740e3dcf4426c5bd709221644
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-05-08 18:10:32 +0800

    check and print if number is unique or a duplicate

--- ask_user_for_for_valid_nbumber_inputs_and_print_the_highest_number.py
+++ ask_user_for_for_valid_nbumber_inputs_and_print_the_highest_number.py
@@ -6,4 +6,8 @@ while True:
     num = int(input("Enter a number:"))
     numbers.append(num)
 #add a checker if number is a duplicate or if its unique
+    if numbers.count(num) > 1:
+        print("Duplicate")
+    else:
+        print("Unique")
 #if user enters an invalid input, break
