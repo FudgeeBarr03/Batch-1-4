f379055e36dac89f84cc609af52961c1d86d219c
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-06-08 23:53:43 +0800

    asks for user input

--- input_10_numbers_and_show_duplicates.py
+++ input_10_numbers_and_show_duplicates.py
@@ -1,4 +1,9 @@
+numbers = []
+
 #ask for user input
-#store user input
+for i in range(10):
+    num = input("Enter a number: ")
+    #store user input
+    numbers.append(num)
 #check for duplicates
 #print the duplicates
