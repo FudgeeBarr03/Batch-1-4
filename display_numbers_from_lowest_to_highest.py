291078f3a994f6ed40d708336a255919a26ca480
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-06-08 23:36:42 +0800

    Added while loop and checks for valid input and fixed sort missing ()

--- display_numbers_from_lowest_to_highest.py
+++ display_numbers_from_lowest_to_highest.py
@@ -1,13 +1,18 @@
 numbers = []
 
-#ask for user input
-num = float(input("Enter a number: "))
-#add numbers to list
-numbers.append(num)
+#create loop to check if user input is valid
+while True:
+    try:
+        #ask for user input
+        num = float(input("Enter a number: "))
+        #add numbers to list
+        numbers.append(num)
+    except ValueError:
+        break
 
 #sort numbers
 if numbers:
-    numbers.sort
+    numbers.sort()
     print("The numbers in ascending order:")
     #print sorted numbers
     for n in numbers:
