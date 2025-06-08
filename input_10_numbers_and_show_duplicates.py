9f7116932f008f7f4085927cb6dffbbbaba9d3d0
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-06-08 23:54:58 +0800

    check for the duplicate numbers

--- input_10_numbers_and_show_duplicates.py
+++ input_10_numbers_and_show_duplicates.py
@@ -5,5 +5,10 @@ for i in range(10):
     num = input("Enter a number: ")
     #store user input
     numbers.append(num)
+
+duplicate = []
 #check for duplicates
+for num in numbers:
+    if numbers.count(num) > 1 and num not in duplicate:
+        duplicate.append(num)
 #print the duplicates
