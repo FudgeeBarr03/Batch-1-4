ef98b6d952a8e6673b955610619ba5f32206cbd7
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-06-09 00:20:42 +0800

    Ask and store user input

--- show_valid_user_input_in_descending_order.py
+++ show_valid_user_input_in_descending_order.py
@@ -1,4 +1,9 @@
+numbers = []
+
 #ask for user input
+num = float(input("Enter a number: "))
 #store numbers
+numbers.append(num)
+
 #use sort() to arrange numbers
 #print  the numbers in descending order
