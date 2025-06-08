4d93a86a8a8578c621ed35218c2ff6476ca5769e
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-06-08 22:57:47 +0800

    print the lowest number out of the inputs that user gave

--- display_the_lowest_number_from_the_user_inputs.py
+++ display_the_lowest_number_from_the_user_inputs.py
@@ -3,10 +3,14 @@ numbers = []
 #Ask for user's input numbers
 while True:
     try:
-        num = float(input("enter a number"))
+        num = float(input("Enter a number: "))
         #add numbers into empty list
         numbers.append(num)
     except ValueError:
         break 
     
 #print the lowest number
+if numbers:
+    print("Lowest number entered:", min(numbers))
+else:
+    print("No valid numbers were entered.")
